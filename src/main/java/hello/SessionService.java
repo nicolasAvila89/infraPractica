6df24b972dc5dadb580358b05d1a93d5204f0a60
Diego Dakszewicz <diego.dakszewicz@nubing.net>
2019-08-27 03:36:49 -0300

    Halzecast replication

--- src/main/java/hello/SessionService.java
+++ src/main/java/hello/SessionService.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.catalina.session.StandardSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import com.hazelcast.core.HazelcastInstance;
@@ -13,47 +16,57 @@ import com.hazelcast.core.HazelcastInstance;
 import javax.servlet.http.HttpSession;
 
 @Service
+@PropertySource("classpath:app.properties")
+
 public class SessionService {
 
     @Autowired
     HttpSession session;
 
+    @Value("${sessionEnabled}")
+    private boolean sessionEnabled;
+
+
     List<byte[]> memoryFiller = new ArrayList<>();
 
-    private static String MARKERS="MARKERS";
-    private static String HITS="HITS";
+    private static String MARKERS = "MARKERS";
+    private static String HITS = "HITS";
 
-    public Integer addHit(){
-        Integer hit= (Integer) session.getAttribute(HITS);
-        if (hit==null){
-            hit=0;
-        }else{
+    public Integer addHit() {
+        Integer hit = (Integer) getSession().getAttribute(HITS);
+        if (hit == null) {
+            hit = 0;
+        } else {
             hit++;
         }
-        session.putValue(HITS, hit);
+        getSession().putValue(HITS, hit);
         return hit;
     }
 
     public void saveMarker(Integer value) {
         memoryFill();
-        session.putValue(MARKERS, value);
+        getSession().putValue(MARKERS, value);
     }
 
     public Integer getMarkers() {
         Integer result = (Integer) session.getAttribute(MARKERS);
-        if (result==null) return 0;
+        if (result == null) return 0;
         return result;
     }
 
     private void memoryFill() {
-        for (int i = 0; i < 100; i++) {
-            byte bytes[] = new byte[1048576];
-            memoryFiller.add(bytes);
+        if (sessionEnabled){
+            for (int i = 0; i < 100; i++) {
+                byte bytes[] = new byte[1048576];
+                memoryFiller.add(bytes);
+            }
         }
     }
 
-    public HttpSession getSession(){
+    public HttpSession getSession() {
+        if (!sessionEnabled){
+            this.session.invalidate();
+        }
         return this.session;
     }
-
 }
